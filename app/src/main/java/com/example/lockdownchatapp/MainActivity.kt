a71ec7ed7851a044576a2367055e066281b67284
Rajneesh <Rajneesh.saini@stigasoft.com>
2020-05-09 18:11:31 +0530

    - Designed the Login Screen

--- app/src/main/java/com/example/lockdownchatapp/MainActivity.kt
+++ app/src/main/java/com/example/lockdownchatapp/MainActivity.kt
@@ -1,23 +1,16 @@
 package com.example.lockdownchatapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -46,7 +39,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginClickNavHeader(view: View) {
-        Toast.makeText(this, "Clicked loginClickNavHeader fun..", Toast.LENGTH_LONG).show()
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+       // Toast.makeText(this, "Clicked loginClickNavHeader fun..", Toast.LENGTH_LONG).show()
+    }
+    fun homeLoginClicked(view: View) {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        // Toast.makeText(this, "Clicked loginClickNavHeader fun..", Toast.LENGTH_LONG).show()
     }
 
     fun addChannelButton(view: View) {
